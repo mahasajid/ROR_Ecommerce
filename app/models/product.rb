2d2c353e4347d4a35b155d8c722a56781d60fd47
Maha Sajid <mahasajid1@gmail.com>
2022-07-15 17:42:13 +0500

    setup

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
+require 'elasticsearch/model'
 class Product < ApplicationRecord
   has_one_attached :image
   has_many :line_items
 
   validates :name, presence: true
   validates :price, presence: true
+
+
+include Elasticsearch::Model
+include Elasticsearch::Model::Callbacks 
+
+
 end
