6fd73d31a258f9d7d2f5bed4fe30e0dcf1408d27
Maha Sajid <mahasajid1@gmail.com>
2022-07-18 12:50:16 +0500

    Elastic Search

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,61 @@ include Elasticsearch::Model
 include Elasticsearch::Model::Callbacks 
 
 
+settings index: {number_of_shards: 1} do
+  mapping dynamic: 'false' do
+  indexes :id, type: :integer
+  indexes :name, type: :keyword, analyzer: 'english'
+  indexes :price, type: :half_float
+  indexes :description, type: :keyword
+  indexes :stock , type: :integer
 end
+end
+
+
+
+def self.search(query)
+  __elasticsearch__.search(
+  {
+    query: {
+       multi_match: {
+         query: query,
+         fields: ['name^5', 'description']
+       }
+     },
+
+     highlight: {
+      pre_tags: ['<mark>'],
+      post_tags: ['</mark>'],
+        fields: {
+          name: {},
+          description: {},
+        }
+          },
+
+
+          suggest: {
+          text: query,
+          title: {
+          term: {
+            size: 1,
+            field: :name
+          }
+          },
+        body: {
+          term: {
+            size: 3,
+            field: :description
+          }
+          }
+          }
+
+
+
+  })
+end
+
+
+
+end
+
+Product.import
