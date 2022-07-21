8124b899496002aa2834b9b72449fcd6d385214c
maha-sajid <maha.sajid@pakeventures.com>
2022-07-21 14:56:47 +0500

    merging

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,6 +310,5 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
   config.navigational_formats = ['*/*', :html, :turbo_stream]
 
-  
-  #config.omniauth :google_oauth2, , 
-end
+  #config.omniauth :google_oauth2, 
+end 
