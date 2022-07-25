b94eea39835466822921baf47622ba7e9e0dc784
maha-sajid <maha.sajid@pakeventures.com>
2022-07-25 16:46:05 +0500

    ssl

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -174,7 +174,7 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  :secure => false
+  :secure = false
   # config.rememberable_options = {}
 
   # ==> Configuration for :validatable
