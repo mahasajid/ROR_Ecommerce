668f9c6974944edc9408b5a3c3583f5b614868d4
maha-sajid <maha.sajid@pakeventures.com>
2022-07-25 17:46:49 +0500

    ssl

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RoRApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.active_job.queue_adapter = :sidekiq
+    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
     # Configuration for the application, engines, and railties goes here.
     #
